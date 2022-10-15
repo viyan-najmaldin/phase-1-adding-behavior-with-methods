450cf9c59a5a7d7e7799c590b1098e6d52ff13ee
viyan-najmaldin <najmadinviyan@gmail.com>
2022-10-16 00:22:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+
+class Cat {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+  let cat = new Cat("Sasha", "female");
+  cat.speak();
+  class Dog {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  let dog = new Cat("Rufio", "male");
+  dog.speak();
+  class Bird {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+    speak() {
+      if (this.sex == "male") {
+        return `It's me! ${this.name}, the parrot!`;
+      } else {
+        return `${this.name} says squawk!`;
+      }
+    }
+  }
+  let bird = new Cat("Pablo", "male");
+  bird.speak();
+  let bird2 = new Cat("Mable", "female");
+  bird2.speak();
